605b0d464c59354de3189925e8d2770059a382a9
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-05-16 09:34:50 -0500

    Parameterized Queries filter posts list

---
+++ src/react-query/hooks/usePost.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+}
+
+const usePost = (userId: number | undefined) => {
+    const fetchPosts = () =>
+        axios
+          .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+                userId
+            }
+          })
+          .then(res => res.data);
+
+    return useQuery<Post[], Error>({
+        queryKey: userId ? ['users', userId,'posts'] : ['posts'],
+        queryFn: fetchPosts,
+        staleTime: 10 * 1000
+    })
+}
+
+export default usePost;

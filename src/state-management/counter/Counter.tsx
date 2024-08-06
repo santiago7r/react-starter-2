525b174c1a7a27733fb5ad37d5aa4089614e968a
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-06 16:51:04 -0500

    Refactor counter

--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,5 +1,14 @@
 import { useReducer, useState } from 'react';
-import counterReducer from './reducers/counterReducer';
+
+interface Action {
+  type: 'INCREMENT' | 'RESET'
+}
+
+const counterReducer = (state: number, action: Action) => {
+  if (action.type === 'INCREMENT') return state + 1;
+  if (action.type === 'RESET') return 0;
+  return state;
+}
 
 const Counter = () => {
   const [value, dispatch] = useReducer(counterReducer, 0);

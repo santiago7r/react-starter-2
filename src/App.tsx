9a853a75bcca0e8f32089498b5e5d7d53aad654e
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-06 12:06:27 -0500

    custom hook for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import { useReducer } from 'react';
 import './App.css';
-import PostList from './react-query/PostList';
-import TodoForm from './react-query/TodoForm';
-import TodoList from './react-query/TodoList';
-import Counter from './state-management/Counter';
-import LoginStatus from './state-management/LoginStatus';
-import TaskList from './state-management/TaskList';
-import taskListReducer from './state-management/reducers/taskListReducer';
-import TasksContext from './state-management/contexts/tasksContext';
-import NavBar from './state-management/NavBar';
-import HomePage from './state-management/HomePage';
-import loginReducer from './state-management/reducers/loginReducer';
-import AuthContext from './state-management/contexts/authContext';
 import AuthProvider from './state-management/AuthProvider';
+import TasksContext from './state-management/contexts/tasksContext';
+import HomePage from './state-management/HomePage';
+import NavBar from './state-management/NavBar';
+import taskListReducer from './state-management/reducers/taskListReducer';
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(taskListReducer, []);

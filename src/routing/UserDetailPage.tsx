f56e15e9b14c03770d2173b32cefc886a281a27f
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-09 15:47:34 -0500

    useParams, useSearchParams and useLocation

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,11 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(searchParams.get('name'));
+
   return <p>User</p>;
 };
 

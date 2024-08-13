9577357a54f90d2ab7c05379f0c5ece74d260e01
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-12 19:06:19 -0500

    defult path

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
         path: '/',
         element: <Layout />,
         children: [
-            { path: '', element: <HomePage /> },
+            { index: true, element: <HomePage /> },
             { path: 'users', element: <UserListPage /> },
             { path: 'users/:id', element: <UserDetailPage /> },
         ]

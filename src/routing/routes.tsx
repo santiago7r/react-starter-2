529e33f877181e2bc19fd919a45ad9e781af7db1
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-16 21:42:28 -0500

    Add default error page

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,12 +3,14 @@ import HomePage from './HomePage';
 import Layout from './Layout';
 import UserDetail from './UserDetail';
 import UsersPage from './UsersPage';
+import ErrorPage from './ErrorPage';
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             { index: true, element: <HomePage /> },
             { 

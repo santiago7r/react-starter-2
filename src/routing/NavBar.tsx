7b19542ba369fe048d839842490517787bfbf0ab
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-16 21:43:09 -0500

    Fix className for NavLink

--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to='/' className="nave-link">Home</NavLink>
+              <NavLink to='/' className="nav-link">Home</NavLink>
             </li>
             <li className="nav-item">
               <NavLink to='users' className="nav-link">users</NavLink>

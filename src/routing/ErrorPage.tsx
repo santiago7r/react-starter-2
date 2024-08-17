020f21906ae4bd0bec89fe142053bb7a73e22415
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-16 22:01:27 -0500

    Using useRouteError

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,4 +1,9 @@
+import { useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log({error});
+
   return (
     <>
       <h1>Oops...</h1>
